cdbbaab4189ca61e9611072ff848ee29a6b310d6
Lock1 <rizaltanur@outlook.com>
2020-09-23 15:30:05 +0700

    Penggantian nama CoreWindow
    Penggantian nama dan perbaikan susunan kode LinearAlgebra1GUI

--- src/LinearAlgebra1GUI.java
+++ src/LinearAlgebra1GUI.java
@@ -1,22 +1,20 @@
+//TODO Exception handling during failed GUI Library import
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Label;
-
 import org.eclipse.swt.graphics.Image;
-
-import org.eclipse.swt.*;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.events.MouseTrackAdapter;
 import org.eclipse.swt.widgets.Text;
-//import org.eclipse.swt.events.ModifyListener;
-//import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.*;
 
 
-public class CoreWindow {
+public class LinearAlgebra1GUI {
 
 	protected Shell shell;
 	private Text text;
@@ -27,13 +25,13 @@ public class CoreWindow {
 	 */
 	public static void main(String[] args) {
 		try {
-			CoreWindow window = new CoreWindow();
+			LinearAlgebra1GUI window = new LinearAlgebra1GUI();
 			window.open();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 
 	/**
 	 * Open the window.
@@ -59,12 +57,14 @@ public class CoreWindow {
 		shell = new Shell(shell);
 		shell.setSize(450, 300);
 		shell.setText("hehe");
-		
+
 		Label lblClick = new Label(shell, SWT.SHADOW_NONE);
-		Image out = SWTResourceManager.getImage("src\\img\\thonkhmm.gif");
-		Image in = SWTResourceManager.getImage("src\\img\\thonkhmm.jpg");
+		//Image out = SWTResourceManager.getImage("img\\thonkhmm.gif");
+		//Image in = SWTResourceManager.getImage("img\\thonkhmm.jpg");
+		Image out = SWTResourceManager.getImage(LinearAlgebra1GUI.class,"/img/thonkhmm.gif");
+		Image in = SWTResourceManager.getImage(LinearAlgebra1GUI.class,"/img/thonkhmm.jpg");
 		shell.setImage(in);
-		lblClick.setImage(out);
+		lblClick.setImage(in);
 		lblClick.addMouseTrackListener(new MouseTrackAdapter() {
 			@Override
 			public void mouseEnter(MouseEvent e) {
@@ -75,7 +75,7 @@ public class CoreWindow {
 				lblClick.setImage(out);
 			}
 		});
-		
+
 		lblClick.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
@@ -85,11 +85,11 @@ public class CoreWindow {
 				kek.open();
 			}
 		});
-		lblClick.setBounds(23, 27, 170, 150);
-		
+		lblClick.setBounds(32, 48, 170, 150);
+
 		text = new Text(shell, SWT.BORDER);
 		text.setBounds(266, 70, 76, 21);
-		
+
 		Button btnNein = new Button(shell, SWT.NONE);
 		btnNein.addMouseListener(new MouseAdapter() {
 			@Override
