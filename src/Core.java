9e6e6a7aa1407c5b854ec79fd65d3c2ddec1f0ce
Lock1 <rizaltanur@outlook.com>
2020-09-23 15:27:16 +0700

    Pembuatan kode program utama
    Core main() menerima string args opsional untuk menjalankan GUI atau CLI. Penggunaan Exception handling ketika gagal menguji args atau load library GUI.

--- src/Core.java
+++ src/Core.java
@@ -1,10 +1,37 @@
+
+
 public class Core {
 
 	public static void main(String[] args) {
-		System.out.println("Heyo");
-//		CoreWindow kul = new CoreWindow();
-//		kul.open();
 		
+		try {
+			if (args[0].equals("-GUI")) {
+				//  --- GUI Part ---
+				try {
+				 	LinearAlgebra1GUI kul = new LinearAlgebra1GUI();
+				 	kul.open();
+				}
+				catch (NoClassDefFoundError e) {
+					System.out.println("Exception : Failed to start GUI\nStarting CLI...\n");
+					LinearAlgebra1CLI.main(args);
+				}
+			}
+			else if (args[0].equals("-CLI")) {
+				// --- CLI Part ---
+				LinearAlgebra1CLI.main(args);
+			}
+			else {
+				System.out.println("Unknown argument");
+				System.out.println("Usage: Core [Interface]");
+				System.out.println("Interface Options :");
+				System.out.println("    -CLI		By default, CLI argument are used");
+				System.out.println("    -GUI		Using Eclipse WindowBuilder libraries");
+			}
+		} 
+		catch (Exception e) {
+			// --- CLI Part ---
+			LinearAlgebra1CLI.main(args);
+		}
 	}
 
 }
