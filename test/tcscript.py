504e26437f610e402bf3f773b23051128f2feb5e
Lock1 <rizaltanur@outlook.com>
2020-10-01 22:31:14 +0700

    Penggantian nama log testfile
    Penggantian nama dari singkatan menjadi lengkap

--- test/tcscript.py
+++ test/tcscript.py
@@ -1,15 +1,15 @@
 # 13519214
 # Linux command
 # Put script in main repository folder, and run with following commands
-# python scriptsaveus.py | java <class file> | tee testlog.txt
+# python tcscript.py | java <class file> | tee testlog.txt
 
 
 # Linear Equation Test Case Check
 dir = "test/"
 pfx = "tc_"
-flnm = ["1a","1b","1c","1d_hb1","1d_hb2","2a","2b","3a","3b"]
-sle = ["_gs","_gj","_inv","_crm"]
-sqrmtx = [1,0,0,1,1,1,0,1,0]
+flnm = ["1a","1b","1c","1d_hb1","1d_hb2","2a","2b","3a","3b","4"]
+sle = ["_gauss","_gaussjordan","_inverse","_cramer"]
+sqrmtx = [1,0,0,1,1,1,0,1,0,1]
 
 for a in range(len(flnm)):
     for i in range(4):
